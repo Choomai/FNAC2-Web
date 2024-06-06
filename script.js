2f6b7d1ce19f6e1103ca48526e8959da9097cb25
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-06-06 07:08:41 +0700

    last edit before restart project with Svelte

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-const backgroundContainer = $("div.background");
-const leftDiv = $("div.left");
-const rightDiv = $("div.right");
-const scrollThreshold = 10;
+const root = document.querySelector(":root");
+
+const backgroundContainer = document.querySelector("div.background");
+const leftDiv = document.querySelector("div.left");
+const rightDiv = document.querySelector("div.right");
+const scrollThreshold = 80;
 let scrollLoop;
 
-leftDiv.hover(
-    () => { // on hover
-        scrollLoop = setInterval(() => {
-            let currentPos = backgroundContainer.scrollLeft();
-            backgroundContainer.scrollLeft(currentPos - scrollThreshold);
-        }, 10);
-    },
-    () => clearInterval(scrollLoop)
-);
+leftDiv.addEventListener("mouseenter", () => { // on hover
+    scrollLoop = setInterval(() => {
+        let rootStyle = getComputedStyle(root);
+        let currentPos = +rootStyle.getPropertyValue("--bg-x-cords").slice(0, -2);
+        
+        if ((currentPos + scrollThreshold) >= 0) {
+            root.style.setProperty("--bg-x-cords", "0px");
+            return clearInterval(scrollLoop);
+        };
+        root.style.setProperty("--bg-x-cords", `${currentPos + scrollThreshold}px`);
+    }, 200);
+})
+leftDiv.addEventListener("mouseleave", () => clearInterval(scrollLoop))
 
-rightDiv.hover(
-    () => { // on hover
-        scrollLoop = setInterval(() => {
-            let currentPos = backgroundContainer.scrollLeft();
-            backgroundContainer.scrollLeft(currentPos + scrollThreshold);
-        }, 10);
-    },
-    () => clearInterval(scrollLoop) // not hover
-);
+rightDiv.addEventListener("mouseenter", () => { // on hover
+    scrollLoop = setInterval(() => {
+        let rootStyle = getComputedStyle(root);
+        let currentPos = +rootStyle.getPropertyValue("--bg-x-cords").slice(0, -2);
+        
+        if ((currentPos - scrollThreshold) <= -416) {
+            root.style.setProperty("--bg-x-cords", "-416px");
+            return clearInterval(scrollLoop);
+        };
+        root.style.setProperty("--bg-x-cords", `${currentPos - scrollThreshold}px`);
+    }, 200);
+})
+rightDiv.addEventListener("mouseleave", () => clearInterval(scrollLoop))
+
+// Key handler
+// document.addEventListener("keydown", e => {
+//     switch (e.key) {
+//         case " ":
+//             backgroundContainer.style.transform.translate;
+//             $("div.overlay-container#flash").show(0);
+//             break;
+//     }
+// })
