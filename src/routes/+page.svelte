586a8489a498246e259f59d9bf38fbac5ca5f155
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-06-06 16:57:42 +0700

    hmm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    export const prerender = true;
+</script>
+
 <p>Try to zoom out if there is overflow, Pan left or right using <kbd>Shift + Scroll Wheel</kbd></p>
 <iframe src="/game/" width="1024" height="768" frameborder="0" title="Game viewport"></iframe>
 
