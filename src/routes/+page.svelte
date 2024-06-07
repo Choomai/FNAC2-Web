e3d93461b39cf1bfc315a00050d20e31048c127b
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-06-07 13:46:44 +0700

    Added warn, it should fix the autoplay issue, browser is dumb.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <p>Try to zoom out if there is overflow, Pan left or right using <kbd>Shift + Scroll Wheel</kbd></p>
-<iframe src="menu/" width="1024" height="768" frameborder="0" title="Game viewport"></iframe>
+<iframe src="warn/" width="1024" height="768" frameborder="0" title="Game viewport"></iframe>
 
 <style>
     p {margin: .8rem 0;}
---
+++ src/routes/warn/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { goto } from "$app/navigation";
+
+    function dismiss(e) {
+        e.target.style.opacity = 0;
+        setTimeout(() => goto("menu/"), 1000);
+    }
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<main class="full-vp">
+    <img src="imgs/warning.png" on:click={dismiss}>
+</main>
+
+<style>
+    img {transition: opacity 1s ease;}
+</style>
